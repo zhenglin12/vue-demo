32441becd86068c878942b572fd361f02830ae19
linzheng89 <linzheng89@foxmail.com>
2019-06-02 18:33:01 +0800

    feat: add pagination

--- src/mock/task.js
+++ src/mock/task.js
@@ -1,82 +1,91 @@
 import Mock from "mockjs";
 import api from "../api";
 
+const Random = Mock.Random;
+
 Mock.mock(api.task.getTasks, "get", {
   code: 200,
   message: "sql cuowu",
-  content: [
-    {
-      favorited: true,
-      status: "delay",
-      components: ["标签1", "标签2", "标签3", "标签4"],
-      info1: {
-        bugs: {
-          total: 10,
-          remain: 5,
-          rank: "B"
+  content: {
+    "currentPage|+1": 1,
+    total: 232,
+    list: [
+      {
+        id: "id001",
+        favorited: true,
+        status: "delay",
+        components: ["标签1", "标签2", "标签3", "标签4"],
+        info1: {
+          bugs: {
+            total: 10,
+            remain: 5,
+            rank: "B"
+          },
+          newTestCases: 20,
+          newTestCasesRate: 0.53,
+          smokingFailed: 10,
+          regressionTestCases: 10,
+          regressionTestCasesRate: 1
         },
-        newTestCases: 20,
-        newTestCasesRate: 0.53,
-        smokingFailed: 10,
-        regressionTestCases: 10,
-        regressionTestCasesRate: 1
+        info2: [
+          {
+            type: "deploy", // 'deploy', 'benchmark', 'qatest', 'smokingtest',
+            name: "job1",
+            status: "pass" // 'pass', 'failed', 'unstable', 'running'
+          },
+          {
+            type: "benchmark", // 'deploy', 'benchmark', 'qatest', 'smokingtest',
+            name: "job2",
+            status: "failed"
+          },
+          {
+            type: "qatest", // 'deploy', 'benchmark', 'qatest', 'smokingtest',
+            name: "job3",
+            status: "unstable"
+          },
+          {
+            type: "smokingtest", // 'deploy', 'benchmark', 'qatest', 'smokingtest',
+            name: "job4",
+            status: "running"
+          }
+        ]
       },
-      info2: [
-        {
-          type: "deploy", // 'deploy', 'benchmark', 'qatest', 'smokingtest',
-          name: "job1",
-          status: "pass" // 'pass', 'failed', 'unstable', 'running'
-        },
-        {
-          type: "benchmark", // 'deploy', 'benchmark', 'qatest', 'smokingtest',
-          name: "job2",
-          status: "failed"
-        },
-        {
-          type: "qatest", // 'deploy', 'benchmark', 'qatest', 'smokingtest',
-          name: "job3",
-          status: "unstable"
-        },
-        {
-          type: "smokingtest", // 'deploy', 'benchmark', 'qatest', 'smokingtest',
-          name: "job4",
-          status: "running"
+      {
+        id: "id002",
+        favorited: false,
+        status: "normal",
+        components: ["标签1", "标签2", "标签3", "标签4"],
+        info1: {
+          bugs: {
+            total: 10,
+            remain: 1,
+            rank: "A"
+          },
+          newTestCases: 20,
+          newTestCasesRate: 0.53,
+          smokingFailed: 10,
+          regressionTestCases: 10,
+          regressionTestCasesRate: 1
         }
-      ]
-    },
-    {
-      favorited: false,
-      status: "normal",
-      info0: ["标签1", "标签2", "标签3", "标签4"],
-      info1: {
-        bugs: {
-          total: 10,
-          remain: 1,
-          rank: "A"
-        },
-        newTestCases: 20,
-        newTestCasesRate: 0.53,
-        smokingFailed: 10,
-        regressionTestCases: 10,
-        regressionTestCasesRate: 1
+      },
+      {
+        id: "id003",
+        favorited: false,
+        status: "finished",
+        components: ["标签1", "标签2", "标签3", "标签4"],
+        info1: {
+          bugs: {
+            total: 10,
+            remain: 5,
+            rank: "C"
+          },
+          newTestCases: 20,
+          newTestCasesRate: 0.53,
+          smokingFailed: 10,
+          regressionTestCases: 10,
+          regressionTestCasesRate: 1
+        }
       }
-    },
-    {
-      favorited: false,
-      status: "finished",
-      info0: ["标签1", "标签2", "标签3", "标签4"],
-      info1: {
-        bugs: {
-          total: 10,
-          remain: 5,
-          rank: "C"
-        },
-        newTestCases: 20,
-        newTestCasesRate: 0.53,
-        smokingFailed: 10,
-        regressionTestCases: 10,
-        regressionTestCasesRate: 1
-      }
-    }
-  ]
+    ]
+  }
 });
--- src/pages/Task/Task.vue
+++ src/pages/Task/Task.vue
@@ -8,7 +8,15 @@
       </el-switch>
       <el-button size="small" type="primary">导出报告</el-button>
     </div>
-    <TaskItem v-if="tasks.length > 0" v-for="(task, index) in tasks" :key="index" :task="task"/>
+    <TaskItem v-if="tasks.length > 0" v-for="(task) in tasks" :key="task.id" :task="task"/>
+    <el-pagination
+      style="text-align: center"
+      @current-change="handleCurrentChange"
+      :current-page.sync="currentPage"
+      :page-size="pageSize"
+      layout="prev, pager, next, jumper"
+      :total="total">
+    </el-pagination>
   </div>
 </template>
 
@@ -16,6 +24,7 @@
 import TaskItem from "./components/TaskItem.vue";
 import axios from "axios";
 import api from "src@/api/task";
+import { constants } from "fs";
 
 export default {
   name: "Task",
@@ -24,15 +33,28 @@ export default {
   },
   data() {
     return {
+      pageSize: 10,
       rangeType: true,
-      tasks: []
+      tasks: [],
+      currentPage: 1,
+      total: 1
     };
   },
   mounted: function() {
-    axios.get(api.getTasks).then(content => {
-      console.log(content);
-      this.tasks = content;
-    });
+    this.getTasks({ currentPage: 1 });
+  },
+  methods: {
+    getTasks: function(params) {
+      const pageSize = this.pageSize;
+      axios.get(api.getTasks, { pageSize, ...params }).then(content => {
+        this.tasks = content.list;
+        this.currentPage = content.currentPage;
+        this.total = content.total;
+      });
+    },
+    handleCurrentChange: function(currentPage) {
+      this.getTasks({ currentPage });
+    }
   }
 };
 </script>
